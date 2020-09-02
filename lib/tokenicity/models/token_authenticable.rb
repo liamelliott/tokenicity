bfce00e77a185b900524e7c8ebbfd7cc0617889f
Liam Elliott <liamelliott1123@gmail.com>
2020-09-02 15:45:07 -0600

    Add TokenAuthenticable Module and Test

---
+++ lib/tokenicity/models/token_authenticable.rb
@@ -0,0 +1,10 @@
+module Tokenicity
+  module Models
+    module TokenAuthenticable
+      extend ActiveSupport::Concern
+
+      class_methods do
+      end
+    end
+  end
+end
---
+++ test/models/token_authenticable_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+module Tokenicity
+  module Models
+    class TokenAuthenticableTest < ActiveSupport::TestCase
+    end
+  end
+end
