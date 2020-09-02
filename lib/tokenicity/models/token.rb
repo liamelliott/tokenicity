fe51e59e608e92e845dea8d6414e404f2dbee91a
Liam Elliott <liamelliott1123@gmail.com>
2020-09-02 17:47:53 -0600

    Add Tests for Token

--- lib/tokenicity/models/token.rb
+++ lib/tokenicity/models/token.rb
@@ -1,6 +1,7 @@
 module Tokenicity
   module Models
     class Token < ActiveRecord::Base
+      self.table_name = :tokenicity_tokens
     end
   end
 end
--- test/models/token_test.rb
+++ test/models/token_test.rb
@@ -3,8 +3,20 @@ require 'test_helper'
 module Tokenicity
   module Models
     class TokenTest < ActiveSupport::TestCase
-      test 'token is' do
-        Token
+      def klass
+        @klass ||= Token
+      end
+
+      def instance
+        @instance ||= klass.new
+      end
+
+      test 'table name is prefixed' do
+        assert_equal('tokenicity_tokens', klass.table_name)
+      end
+
+      test 'allows setting of plaintext token' do
+        assert_respond_to(instance, :plaintext=)
       end
     end
   end
